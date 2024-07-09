7963e4eaed99d910256166c2e56cd4286fb85587
NeoNoir40 <neroxartz@hotmail.com>
2024-07-09 13:28:58 -0600

    cambios wil

---
+++ app/(tabs)/panel.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Panel = () => {
+  return (
+    <View>
+      <Text>Panel</Text>
+    </View>
+  )
+}
+
+export default Panel
---
+++ constants/icons.js
@@ -0,0 +1,8 @@
+import panel  from "../assets/icons/dashboard.png"
+
+
+
+export default {
+    panel,
+}
+    
