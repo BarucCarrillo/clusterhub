fa97aa1f8908684a526c5d32d1e2c21987c32856
Wilberth Antony Cahuich Cruz <22393239@utcancun.edu.mx>
2024-07-19 11:35:54 -0600

    implementacion widgets al crear dashboard

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { BACKEND_API } from '@env'; // Asegúrate de que esta línea esté presente en tu archivo
+import { BACKEND_API } from "@env"; // Asegúrate de que esta línea esté presente en tu archivo
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Alert } from "react-native";
@@ -14,9 +14,6 @@ const GlobalProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-
-
-
   const loginRequest = async (data) => {
     try {
       const response = await login(data); // Obtén la respuesta de la función login
@@ -27,21 +24,20 @@ const GlobalProvider = ({ children }) => {
         await AsyncStorage.setItem("token", response.token);
         Alert.alert("Login successful", `Welcome ${response.nombre}`);
         setIsLogged(true);
-        const userData=({
+        const userData = {
           id: response.id,
           nombre: response.nombre,
           apellidos: response.apellidos,
           correo: response.correo,
-        });
+        };
         await AsyncStorage.setItem("user", JSON.stringify(userData));
         Alert.alert("Login successful", `Welcome ${response.nombre}`);
         setIsLogged(true);
         setUser(userData);
-        console.log('User after login:', userData);
+        console.log("User after login:", userData);
       } else {
         Alert.alert("Login failed", response.error || "Unknown error");
       }
-
     } catch (error) {
       console.error("Login error", error);
       Alert.alert("Login error", error.message || "Unknown error");
@@ -54,7 +50,7 @@ const GlobalProvider = ({ children }) => {
       if (!token) {
         throw new Error("No token found. Please login again.");
       }
-  
+
       const response = await fetch(`${BACKEND_API}/users/${data.id}`, {
         method: "PUT",
         headers: {
@@ -62,14 +58,14 @@ const GlobalProvider = ({ children }) => {
         },
         body: JSON.stringify(data),
       });
-  
+
       if (!response.ok) {
         const errorData = await response.json();
         throw new Error(errorData.error || "Update failed");
       }
-  
+
       const result = await response.json();
-  
+
       // Asumir que la actualización fue exitosa si llegamos a este punto
       // Actualizar la información del usuario en AsyncStorage con los datos que enviaste
       const updatedUserData = {
@@ -79,36 +75,36 @@ const GlobalProvider = ({ children }) => {
         correo: data.correo,
       };
       await AsyncStorage.setItem("user", JSON.stringify(updatedUserData));
-  
+
       Alert.alert("Update successful", "User information updated successfully");
 
       setUser(updatedUserData);
-      console.log('User after update:', updatedUserData);
-      
+      console.log("User after update:", updatedUserData);
+
       return result;
-  
     } catch (error) {
       console.error("Error:", error);
       Alert.alert("Update error", error.message || "Unknown error");
       throw error;
     }
   };
-  
-
 
   const userChangePassword = async (data) => {
-    try{
+    try {
       const token = await AsyncStorage.getItem("token");
       if (!token) {
         throw new Error("No token found. Please login again.");
       }
-      const response = await fetch(`${BACKEND_API}/userChangePassword/${user.id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+      const response = await fetch(
+        `${BACKEND_API}/userChangePassword/${user.id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
       if (!response.ok) {
         const errorData = await response.json();
         throw new Error(errorData.error || "Update failed");
@@ -116,18 +112,45 @@ const GlobalProvider = ({ children }) => {
       const result = await response.json();
       Alert.alert("Update successful", "User information updated successfully");
       return result;
-    }catch (error) {
+    } catch (error) {
       console.error("Error:", error);
       Alert.alert("Update error", error.message || "Unknown error");
       throw error;
     }
+  };
 
+  const insertWidgetsInDashboard = async (id,data) => {
+    try {
+      const token = await AsyncStorage.getItem("token"); // Obtener el token almacenado
+      if (!token) {
+        throw new Error("No token found. Please login again.");
+      }
 
-  }
+      const response = await fetch(`${BACKEND_API}/dashboard_graficas`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: id,
+          graficas_id: data,
+        }),
+      });
 
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || "Dashboard creation failed");
+      }
 
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
 
-  
+  };
+
   const logout = async () => {
     try {
       await AsyncStorage.removeItem("token");
@@ -140,10 +163,7 @@ const GlobalProvider = ({ children }) => {
       console.error("Logout error", error);
       Alert.alert("Logout error", error.message || "Unknown error");
     }
-  }
-
-
-  
+  };
 
   useEffect(() => {
     const checkLogin = async () => {
@@ -169,15 +189,24 @@ const GlobalProvider = ({ children }) => {
     checkLogin();
   }, []);
 
-
   useEffect(() => {
-    console.log('User state updated:', user);
+    console.log("User state updated:", user);
   }, [user]);
-
 
   return (
     <GlobalContext.Provider
-      value={{ isLogged, setIsLogged, user, setUser, loading, loginRequest ,logout, updateInfoUser,userChangePassword}}
+      value={{
+        isLogged,
+        setIsLogged,
+        user,
+        setUser,
+        loading,
+        loginRequest,
+        logout,
+        updateInfoUser,
+        userChangePassword,
+        insertWidgetsInDashboard,
+      }}
     >
       {children}
     </GlobalContext.Provider>
